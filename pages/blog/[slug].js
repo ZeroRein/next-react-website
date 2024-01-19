03e649bff723b5d34f4e500c2bf5fad407ba04b0
ZeroRein <131316771+ZeroRein@users.noreply.github.com>
2024-01-19 16:26:51 +0900

    096

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -8,6 +8,7 @@ import PostBody from 'components/post-body'
 import TwoColumn from 'components/two-column'
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
+import Pagination from 'components/pagination'
 import Image from 'next/image'
 import { getPlaiceholder } from 'plaiceholder'
 
@@ -21,6 +22,8 @@ export default function Post({
     eyecatch,
     categories,
     description,
+    prevPost,
+    nextPost,
 }) {
     return (
     <Container>
@@ -59,6 +62,12 @@ export default function Post({
                     <PostCategories categories={categories} />
                 </TwoColumn.Sidebar>
             </TwoColumn>
+            <Pagination
+            prevText={prevPost.title}
+            prevUrl={`/blog/${prevPost.slug}`}
+            nextText={nextPost.title}
+            nextUrl={`/blog/${nextPost.slug}`}
+            />
         </article>
         
     </Container>
@@ -66,11 +75,11 @@ export default function Post({
 }
 
 export async function getStaticPaths() {
-    const allSlugs = await getAllSlugs()
+    const allSlugs = await getAllSlugs(5)
 
     return {
-        paths: allSlugs.map(({ slug}) => `/blog/${slug}`),
-        fallback: false,
+        paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+        fallback: 'blocking',
     }
 }
 
@@ -78,25 +87,29 @@ export async function getStaticProps(context) {
     const slug = context.params.slug
 
     const post = await getPostBySlug(slug)
-    const description = extractText(post.content)
-    const eyecatch = post.eyecatch ?? eyecatchLocal
-    const { base64 } = await getPlaiceholder(eyecatch.url)
-    eyecatch.blurDataURL = base64
-
-    const allSlugs = await getAllSlugs()
-    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
-
-    return {
-        props: {
-            title: post.title,
-            publish: post.publishDate,
-            content: post.content,
-            eyecatch: post.eyecatch,
-            eyecatch: eyecatch,
-            categories: post.categories,
-            description: description,
-            prevPost: prevPost,
-            nextPost: nextPost,
-        },
+    if (!post) {
+        return { notFound: true }
+    } else {
+        const description = extractText(post.content)
+        const eyecatch = post.eyecatch ?? eyecatchLocal
+        const { base64 } = await getPlaiceholder(eyecatch.url)
+        eyecatch.blurDataURL = base64
+        
+        const allSlugs = await getAllSlugs()
+        const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+        
+        return {
+            props: {
+                title: post.title,
+                publish: post.publishDate,
+                content: post.content,
+                eyecatch: post.eyecatch,
+                eyecatch: eyecatch,
+                categories: post.categories,
+                description: description,
+                prevPost: prevPost,
+                nextPost: nextPost,
+            },
+        }
     }
-}
+    }
